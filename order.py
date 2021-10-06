4aa8d206ac67b4fe6ba0eddcb6a0d4759e11b0cd
ianatolij020@gmail.com <ianatolij020@gmail.com>
2021-10-06 17:22:42 +0300

    1z ready

--- order.py
+++ order.py
@@ -11,30 +11,3 @@ for i in range(len(data)):
 	#print(c)
 	if 2 in c.values():
 		print(data[i])
-	
-		
-	
-#print(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Symbol = ["о", "О","o", "O" ]
-# with open ("C.txt", "r") as myfile:
-#     data = myfile.read().replace('\n', '')
-#     print(data)
-#     freq = 0
-#     for char in data:
-#         if char == Symbol:
-#             freq = freq + 1
-#     print(freq)
