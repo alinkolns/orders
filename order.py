9c5c2e237dbc0be65502940aca1fe1eb6cd93d31
roman-pixel <63606348+roman-pixel@users.noreply.github.com>
2021-10-06 15:51:31 +0300

    Update order.py

--- order.py
+++ order.py
@@ -1,1 +1,14 @@
-dfgdfg
+from random import randint
+
+rnd = randint(2, 3)
+#print(rnd)
+
+Symbol = ["о", "О","o", "O" ]
+with open ("C.txt", "r") as myfile:
+    data = myfile.read().replace('\n', '')
+    print(data)
+    freq = 0
+    for char in data:
+        if char == Symbol:
+            freq = freq + 1
+    print(freq)
