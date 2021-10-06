2befcd1fc0b7f168da65ca0af86e2d1cc2e860fa
ianatolij020@gmail.com <ianatolij020@gmail.com>
2021-10-06 16:27:01 +0300

    list_gen

--- order.py
+++ order.py
@@ -1,14 +1,32 @@
-from random import randint
+import random
+random.seed(23)#Зерно
+list_data=[]
+data = [random.randint(pow(10,4),pow(10,5)) for i in range(0, 5)]
 
-rnd = randint(2, 3)
+	
+print(data)	
+
 #print(rnd)
 
-Symbol = ["о", "О","o", "O" ]
-with open ("C.txt", "r") as myfile:
-    data = myfile.read().replace('\n', '')
-    print(data)
-    freq = 0
-    for char in data:
-        if char == Symbol:
-            freq = freq + 1
-    print(freq)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Symbol = ["о", "О","o", "O" ]
+# with open ("C.txt", "r") as myfile:
+#     data = myfile.read().replace('\n', '')
+#     print(data)
+#     freq = 0
+#     for char in data:
+#         if char == Symbol:
+#             freq = freq + 1
+#     print(freq)
