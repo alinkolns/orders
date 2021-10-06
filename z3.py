19b1559649edc31866c45f49a50aa510b4431bd2
ianatolij020@gmail.com <ianatolij020@gmail.com>
2021-10-07 02:29:25 +0300

    z3 ready

--- z3.py
+++ z3.py
@@ -1,20 +1,78 @@
 import random
 import numpy as np
-
+from itertools import groupby
 random.seed(23)#Зерно
+a=[random.randint(100,999) for i in range(50)]
+np.array(a)
+CHPE=1
+#################!!!!!!ЧИСЛО ЗНАЧИТ,ЧТО НУЖНО УДАЛИТЬ СТОБЕЦ В КОТОРОМ ЭЛЕМЕНТ ИМЕЕТ СТОЛЬКО ПРОСТЫХ ДЕЛИТЕЛых!
+massive=np.reshape(a,(5,10))
+print("двумерный масси\n",massive)
+dely=[]
+strofcol=[]
+for n in range(len(massive)):
+    for m in range(len(massive[n])):
+        k=massive[n][m]
+        st=[]
+        print("Делители числа %d:"%k)
+        for i in range(k - 1, 1, -1):
+            is_simple = 0 # Счётчик
+            if (k % i == 0):
+                for j in range(i - 1, 1, -1):
+                    if (i % j == 0):
+                        is_simple = is_simple + 1 # Увеличиваем, если находим делитель
+                if (is_simple == 0): # Если делителей не было найдено, выводим
+                    st.append(i)
+        if len(st)==CHPE: 
+            strofcol.append(m)
+        print(st)
+        print("Число простых делителей = ",len(st))
+        print("\n")
+   
+        st=[]
 
-m, n = 5, 10
-a = [[random.randint(100, 999) for j in range(m)] for i in range(n)]#генерация массива 5х10
-a=np.array(a)
-print("Исходный массив:\n\n", a)
+sc=list(set(strofcol))
+print("столбцы для удаления",sc)
+massive=np.delete(massive,sc,1)
+print(massive)
+        
 
-numb = int(input("Введите целое число: "))
-print("Простые:", end = " ")
-for i in range(numb - 1, 1, -1):
-    is_simple = 0 # Счётчик
-    if (numb % i == 0):
-        for j in range(i - 1, 1, -1):
-            if (i % j == 0):
-                is_simple = is_simple + 1 # Увеличиваем, если находим делитель
-        if (is_simple == 0): # Если делителей не было найдено, выводим
-            print(i, end = " ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
